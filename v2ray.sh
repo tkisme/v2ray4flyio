0fc10e4e0a39d486b4b0bc82ba196ab0b77e3fae
Kevin Tang <tkisme2013@gmail.com>
2023-03-05 23:22:26 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -62,7 +62,7 @@ cat <<EOF >/etc/v2ray/config.json
                         "id": "$UUID",
                         "alterId": 64
                     }
-                ],
+                ]
             },
             "streamSettings": {
                 "network": "ws"
